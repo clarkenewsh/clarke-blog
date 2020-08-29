7d0587cead6dc81d5c128a4fd7dd7a2bbe809c13
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2020-08-29 16:59:58 +0100

    page route 'about' implemnted

---
+++ pages/about.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <div>
+      <p><NLink to="/" class="button--grey">Home</NLink></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+h1 {
+  font-weight: 400;
+}
+.name {
+  color: #00C48D;
+}
+p {
+  margin-top: 20px;
+}
+</style>
