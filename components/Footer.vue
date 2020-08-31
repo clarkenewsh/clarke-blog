37ed594c76a73a4f79ebc11e3a0b3a561a59289e
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2020-08-31 17:55:56 +0100

    styled footer component implemented

---
+++ components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <nuxt-link to="/">
+      Home
+    </nuxt-link>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  props: {
+    // eslint-disable-next-line vue/require-default-prop
+    msg: String
+  }
+}
+
+</script>
+<style>
+footer {
+    height: 8rem;
+    border-top: 5px solid lightseagreen;
+    width: 100%;
+}
+</style>
