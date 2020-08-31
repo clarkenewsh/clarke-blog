01eabfc6cf17eb7c0602b43a6af8d72cd24d0d36
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2020-08-31 17:55:27 +0100

    styled navabar componenet implemented

---
+++ components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="nav">
+    <div class="nav__link">
+      <nuxt-link to="/">
+        Home
+      </nuxt-link>
+    </div>
+    <div class="nav__link">
+      <nuxt-link to="/About">
+        About
+      </nuxt-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  props: {
+    // eslint-disable-next-line vue/require-default-prop
+    msg: String
+  }
+}
+</script>
+<style>
+.nav {
+    display: flex;
+}
+
+.nav__link {
+    padding: 1rem;
+}
+</style>
